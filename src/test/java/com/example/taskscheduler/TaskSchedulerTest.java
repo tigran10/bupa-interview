67d98288b1c3204e0902095e4520500d66bed797
Tigran Mnatsakanyan <129395152+tigranice@users.noreply.github.com>
2025-01-13 14:56:23 +0000

    tests

--- src/test/java/com/example/taskscheduler/TaskSchedulerTest.java
+++ src/test/java/com/example/taskscheduler/TaskSchedulerTest.java
@@ -7,5 +7,15 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskSchedulerTest {
+    @Test
+    void fail() throws InterruptedException {
 
+        assertTrue(false);
+    }
+
+    @Test
+    void pass() throws InterruptedException {
+
+        assertTrue(true);
+    }
 }
